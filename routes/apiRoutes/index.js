e377e2105b460e71c2c74dd0534b228cd2d041f4
Mcrouse <matthew.crouse@temple.edu>
2021-01-25 18:08:03 -0500

    pushing to show LA an issue

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 router.use(require('./voterRoutes'));
-
-
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voteRoutes'));
+
 
 
 
